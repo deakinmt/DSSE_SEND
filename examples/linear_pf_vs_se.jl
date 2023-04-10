c2003acfe7cb4664253e7e1c194a05889747ea06
MartaVanin <marta.vanin@esat.kuleuven.be>
2023-04-10 05:07:22 +0200

    ref test case

--- examples/linear_pf_vs_se.jl
+++ examples/linear_pf_vs_se.jl
@@ -1,69 +1,81 @@
 import DSSE_SEND as _DS
 import CSV
-import Dates
+import Dates, Ipopt
 
-A = Matrix(CSV.read(joinpath(_DS.BASE_DIR, "twin_data/linear_send_network_model/mdl_A.csv"), header=0))
-b = Matrix(CSV.read(joinpath(_DS.BASE_DIR, "twin_data/linear_send_network_model/mdl_b.csv"), header=0))
+include("linear_pf_utils.jl")
 
-vbase = Matrix(CSV.read(joinpath(_DS.BASE_DIR, "twin_data/linear_send_network_model/vbase.csv"), header=0))
-
-p_idx = Matrix(CSV.read(joinpath(_DS.BASE_DIR, "twin_data/linear_send_network_model/power_index.csv"), header=0))
-
-timestep = Dates.DateTime(2022, 05, 13, 12, 00, 00)
+A,b,vbase,v_idx,p_idx, x0 = get_Abvvpx()
 
 x1 = build_x0(p_idx, timestep)
-x0 = Matrix(CSV.read(joinpath(_DS.BASE_DIR, "twin_data/linear_send_network_model/x0.csv"), header=0))
 
-v = A*x1+b
+data  = _DS.default_network_parser(;adjust_tap_settings=true)
+_DS.assign_se_settings!(data)
+_DS.assign_voltage_bounds!(data , vmin=0.8, vmax=1.3)
 
-v./vbase
+time_step_begin = Dates.DateTime(2022, 07, 15, 12, 14, 30)
+time_step_end = time_step_begin+Dates.Minute(10)
+time_step_step = Dates.Minute(2)
+aggregation = time_step_step
 
-function build_x0(p_idx::Matrix{String}, timestep::Dates.DateTime; power_partition::String="balanced")
-    day = Dates.Day(timestep).value
-    month = Dates.Month(timestep).value
-    file = joinpath(_DS.BASE_DIR, "twin_data/telemetry/2022_$(month)_$(day)/all_measurements_$(month)_$(day).csv")
-    df = CSV.read(file)
-    df.p = df.p * 1000 #now in W
-    df.q = df.q * 1000 #now in W
-    ts_df = filter(x->x.IsoDatetime .== timestep, df)
-    x0 = Matrix{Float64}(undef, length(p_idx)*2, 1)
-    pseudos = _DS.get_max_ss_loads()
-    for start_idx in [0, 96] # first 96/ indices are active power, rest is reactive
-        for i in 1:3:Int(length(x0)/2) 
-            idx = lowercase.(p_idx)[i]
-            # phase = Int(idx[end])
-            cmp = occursin("_lv", idx) ? idx[1:end-5] : idx[1:end-2]
-            if cmp == "tx3"
-                p = filter(x->x.Id .== "wt", ts_df).p[1]               
-                q = filter(x->x.Id .== "wt", ts_df).q[1]               
-                PQ1, PQ2, PQ3 = start_idx == 0 ? subdivide_power(p, power_partition) : subdivide_power(q, power_partition)
-            elseif cmp == "tx5"
-                p = filter(x->x.Id .== "storage", ts_df).p[1]               
-                q = filter(x->x.Id .== "storage", ts_df).q[1]               
-                PQ1, PQ2, PQ3 = start_idx == 0 ? subdivide_power(p, power_partition) : subdivide_power(q, power_partition)
-            elseif cmp == "rmu2"
-                p = filter(x->x.Id .== "solar", ts_df).p[1]               
-                q = filter(x->x.Id .== "solar", ts_df).q[1]               
-                PQ1, PQ2, PQ3 = start_idx == 0 ? subdivide_power(p, power_partition) : subdivide_power(q, power_partition)
-            elseif cmp ∈ ts_df.Id
-                usr_df = filter(x->x.Id .== cmp, ts_df)
-                PQ1, PQ2, PQ3 = start_idx == 0 ? subdivide_power(usr_df.p[1], power_partition) : subdivide_power(usr_df.q[1], power_partition)
-            elseif cmp ∈ collect(keys(pseudos))
-                PQ1, PQ2, PQ3 = start_idx == 0 ? fill(pseudos[cmp]*1000/3, 3) : zeros(3) # x1000 so it's in Watts
+exclude = ["ss02", "ss17"]
+_DS.add_measurements!(time_step_begin, data , aggregation, exclude = exclude, add_ss13=true) # this is just to initialize the result dataframe
+meas_names = [meas["name"] isa Vector ? meas["name"][1] : meas["name"] for (_,meas) in data["meas"]]
+
+cols = vcat("timestep", "termination_status", "objective", 
+            [name*"_p1" for name in unique(meas_names)], [name*"_p2" for name in unique(meas_names)], [name*"_p3" for name in unique(meas_names)])
+
+# perturbation percentage 
+pperc = 1.01
+
+align_dsse_and_linear_model!(data)
+
+plot_indices = []
+plot_xticks = []
+for (m, meas) in data["meas"]
+    if meas["name"][1] ∉ vcat(plot_xticks, 's')
+        if meas["name"][1] ∈ ["storage", "wt", "solar"]
+            if meas["name"][1] == "solar" 
+                push!(plot_indices, findfirst(x->occursin("RMU2", x), v_idx))
+            elseif meas["name"][1] == "wt" 
+                push!(plot_indices, findfirst(x->occursin("TX3", x), v_idx))
             else
-                PQ1, PQ2, PQ3 = zeros(3)
+                push!(plot_indices, findfirst(x->occursin("TX5", x), v_idx))
             end
-            x0[start_idx+i]   = PQ1
-            x0[start_idx+i+1] = PQ2
-            x0[start_idx+i+2] = PQ3
+            push!(plot_xticks, meas["name"][1])
+        else
+            push!(plot_indices, findfirst(x->occursin(uppercase(meas["name"][1]), x), v_idx))
+            push!(plot_xticks, meas["name"][1])
         end
     end
-    return replace(x0, NaN=>0)
 end
 
-function subdivide_power(P::Float64, power_partition::String)
-    if power_partition == "balanced"
-        P1, P2, P3 = P/3, P/3, P/3
-    end
-    return P1, P2, P3
+for ts in time_step_begin:time_step_step:time_step_end
+
+    _DS.add_measurements!(ts, data, aggregation, exclude = exclude, add_ss13=true) 
+    
+    se_sol  = _DS.solve_acr_mc_se(data, Ipopt.Optimizer)
+    
+    _DS.post_process_dsse_solution!(se_sol)
+
+    vs = get_voltages_tidy(se_sol, data, plot_xticks, "vd")
+    ps =  get_powers_tidy(se_sol, p_idx, "pd")
+    qs =  get_powers_tidy(se_sol, p_idx, "qd")
+    pp = perturb_powers(ps, pperc)
+    vp = (A*vcat(pp, qs)+b)./vbase
+
+
+    #TODO plot voltages for laod with measurement only
+
 end
+
+# the phase voltages are floating, so comparing the ones from the lpf to those of the state estimation does not make sense!
+# what we could do is run state estimation and then use the derived powers to estimate vm 
+
+ρ  = _DS.get_voltage_residuals_one_ts(data, se_sol, in_volts=false)
+
+vp_idx_p1 = [vp[i] for i in plot_indices]
+vp_idx_p2 = [vp[i[1]+1] for i in plot_indices]
+vp_idx_p3 = [vp[i[1]+2] for i in plot_indices]
+
+scatter(vs[2:3:end])
+scatter!(vp_idx_p1)
